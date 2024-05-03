d7e702dd2970b5b0dd76d42cdd200cdd347f9e83
nabinhamal <nabinhamal2@gmail.com>
2024-05-03 10:05:50 +0545

    .

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,12 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server:{
-    port: 3000,
     proxy:{
       "/api":{
         target: "https://lookscoutnh.vercel.app",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+        secure: false,
       }
     }
   }
