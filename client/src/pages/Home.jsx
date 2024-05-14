1113947022ec34d733b0cda088f7ceb9fd0b8502
Nabin Hamal <68802971+nabinhamal@users.noreply.github.com>
2024-05-14 10:44:16 +0545

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Home = () => {
   return (
-    <section className="h-auto w-auto  bg-[#EAEBF0]">
+    <section className="h-auto w-screen  bg-[#EAEBF0]">
       <div className="flex flex-col justify-center text-center items-center h-screen">
         <h2 className="text-blue-700 text-2xl md:text-3xl lg:text-4xl font-medium uppercase">1% Of the industry</h2>
         <h1 className="md:text-5xl lg:text-6xl text-4xl lg:text-5xl text-black font-bold mt-4">
